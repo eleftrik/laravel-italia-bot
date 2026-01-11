a34e0595bce02b77653a83b306cdc77514c2df15
Erik D'Ercole <erik.dercole@encodia.it>
2026-01-11 19:20:45 +0100

    chore: add PEST architecture tests
    - ensure dd, die, ds, etc. are not used
    - force strict types

---
+++ tests/Architecture/ArchitectureTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Architectural tests', function (): void {
+    arch()
+        ->preset()
+        ->php();
+
+    arch()
+        ->preset()
+        ->security();
+
+    arch()
+        ->expect('App')
+        ->toUseStrictTypes()
+        ->not->toUse(['ds', 'dsq', 'print_r', 'sleep']);
+});
+
+
+
+
