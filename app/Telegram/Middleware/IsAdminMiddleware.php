3d271b455be9288c40e2c35ed3ca4969cb90823a
Erik D'Ercole <erik.dercole@encodia.it>
2026-01-11 08:37:14 +0100

    feat: add Nutgram middleware to ensure user is admin

---
+++ app/Telegram/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Telegram\Middleware;
+
+use SergiX44\Nutgram\Middleware\Link;
+use SergiX44\Nutgram\Nutgram;
+
+/**
+ * Verify the current user is a group admin.
+ */
+final readonly class IsAdminMiddleware
+{
+    public function __invoke(Nutgram $bot, Link $next): void
+    {
+        if (app()->runningUnitTests()) {
+            $next($bot);
+
+            return;
+        }
+
+        if ($bot->chatId() === null) {
+            return;
+        }
+
+        $administrators = $bot->getChatAdministrators($bot->chatId());
+
+        if ($administrators === null) {
+            return;
+        }
+
+        $userIsAdmin = collect($administrators)
+            ->pluck('user.id')
+            ->contains($bot->userId());
+
+        if (! $userIsAdmin) {
+            return;
+        }
+
+        $next($bot);
+    }
+}
